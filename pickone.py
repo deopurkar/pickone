7b73d7f249145ec50a19816101be22af4be3880b
Anand Deopurkar (proteus) <anandrdeopurkar@gmail.com>
2022-11-11 00:00:36 +1100

    fixed read/write

--- pickone.py
+++ pickone.py
@@ -2,14 +2,22 @@ import csv
 import random
 
 def updateScores(choices, choice):
+    with open("namesdb.csv", 'r') as file:
+      csvreader = csv.DictReader(file,fieldnames=['Name','Score'])
+      allNames = list(csvreader)
+
+    for i in range(0, len(choices)):
+        allNames.remove(choices[i])
+        if i == choice:
+            choices[i]['Score'] = float(choices[i]['Score']) * 1.1
+        else:
+            choices[i]['Score'] = float(choices[i]['Score']) / 1.1
+        allNames.append(choices[i])
+
     with open("namesdb.csv", 'w') as file:
         csvwriter = csv.DictWriter(file, fieldnames=['Name','Score'])
-        for i in range(0, len(choices)):
-            if i == choice:
-                choices[i]['Score'] = float(choices[i]['Score']) * 1.1
-            else:
-                choices[i]['Score'] = float(choices[i]['Score']) / 1.1
-            csvwriter.writerow(choices[i])
+        for name in allNames:
+            csvwriter.writerow(name)
 
 def getRandomElement(lst):
     totalScore = sum([float(pair['Score']) for pair in lst])
@@ -24,9 +32,9 @@ def getRandomElement(lst):
 def askAndUpdate():
   with open("namesdb.csv", 'r') as file:
       csvreader = csv.DictReader(file,fieldnames=['Name','Score'])
-      lst = list(csvreader)
-  choice1 = getRandomElement(lst)
-  choice2 = getRandomElement(lst)
+      allNames = list(csvreader)
+  choice1 = getRandomElement(allNames)
+  choice2 = getRandomElement(allNames)
   choice = int(input("1. " + choice1['Name'] +"  2. " + choice2['Name'] +" : "))
   if choice == 0:
     return False
