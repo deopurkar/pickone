9751e3a17c78621b8537b5369701974af74790a3
Anand Deopurkar (proteus) <anandrdeopurkar@gmail.com>
2022-11-10 23:42:14 +1100

    logarithmic scoring

--- pickone.py
+++ pickone.py
@@ -6,17 +6,27 @@ def updateScores(choices, choice):
         csvwriter = csv.DictWriter(file, fieldnames=['Name','Score'])
         for i in range(0, len(choices)):
             if i == choice:
-                choices[i]['Score'] = int(choices[i]['Score']) + 1
+                choices[i]['Score'] = float(choices[i]['Score']) * 1.1
             else:
-                choices[i]['Score'] = int(choices[i]['Score']) - 1
+                choices[i]['Score'] = float(choices[i]['Score']) / 1.1
             csvwriter.writerow(choices[i])
+
+def getRandomElement(lst):
+    totalScore = sum([float(pair['Score']) for pair in lst])
+    runningScore = 0
+    i = 0
+    randomScore = random.random() * totalScore
+    while runningScore < randomScore:
+        i = i + 1
+        runningScore = runningScore + float(lst[i]['Score'])
+    return lst[max(i-1,0)]
 
 def askAndUpdate():
   with open("namesdb.csv", 'r') as file:
       csvreader = csv.DictReader(file,fieldnames=['Name','Score'])
       lst = list(csvreader)
-  choice1 = random.choice(lst)
-  choice2 = random.choice(lst)
+  choice1 = getRandomElement(lst)
+  choice2 = getRandomElement(lst)
   choice = int(input("1. " + choice1['Name'] +"  2. " + choice2['Name'] +" : "))
   if choice == 0:
     return False
